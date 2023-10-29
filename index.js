26a3b1eb03586174ff25f971bcb80d966419b347
Replit user <>
2023-10-29 07:52:59 +0000

    feat: implement /api/whoami endpoint

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ A request to /api/whoami should return
 - a JSON object with your preferred language in the language key.
 - a JSON object with your software in the software key.
 */
+app.get('/api/whoami', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress: ip, language: language, software: software });
+});
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
